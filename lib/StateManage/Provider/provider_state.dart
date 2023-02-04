041b1fe58f383c5ac377ad4593c1e411b32fed7a
my0366 <rlacnrrnrhd@naver.com>
2023-02-04 14:35:57 +0900

    NOM-110 Provider 구조 재구성 fix

--- lib/StateManage/Provider/provider_state.dart
+++ lib/StateManage/Provider/provider_state.dart
@@ -2,52 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'provider_provider.dart';
 
-class Button extends StatefulWidget {
-  const Button({Key? key}) : super(key: key);
-
-  @override
-  State<Button> createState() => _ButtonState();
-}
-
-class _ButtonState extends State<Button> {
-  bool isEnabled = false;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-  bool isDecreaseButtonDisabled = true;
+class Counter extends StatelessWidget {
+  const Counter({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ElevatedButton(
-            key: const Key("increase Button"),
-            onPressed: () {
-              context.read<Counts>().increase();
-              isDecreaseButtonDisabled = false;
-            },
-            child: const Icon(Icons.add)),
-        const SizedBox(
-          width: 40,
-        ),
-        ElevatedButton(
-            key: const Key("decrease Button"),
-
-            onPressed: () {
-              print(isDecreaseButtonDisabled);
-              isDecreaseButtonDisabled ? null : context.read<Counts>().decrease();
-              if (context.read<Counts>().count == 0) {
-                isDecreaseButtonDisabled = true;
-              } else {
-                isDecreaseButtonDisabled = false;
-              }
-
-            },
-            child: const Icon(Icons.remove))
-      ],
+    return Text(
+      "Count = ${context.watch<Counts>().count}",
+      key: const Key("result Text"),
+      style: const TextStyle(
+        fontSize: 20,
+      ),
     );
   }
 }
